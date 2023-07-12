34b24bf2ec83c17e04dbc67c05d41def5c74c7b6
{wonhee} <wanny416@sookmyung.ac.kr>
2023-07-13 03:23:42 +0900

    feat : 리postadd review on post page

--- review/models.py
+++ review/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Review(models.Model):
-    post = models.ForeignKey('posts.Post', on_delete=models.CASACDE) # 글
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE) # 글
     reviewText=models.TextField() #리뷰 댓글
