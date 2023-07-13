23294747fd6d47df043793442e6a41203485aa91
{wonhee} <wanny416@sookmyung.ac.kr>
2023-07-13 21:04:07 +0900

    feat: post detail, review model modify

--- review/models.py
+++ review/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Review(models.Model):
     post = models.ForeignKey('posts.Post', on_delete=models.CASCADE) # 글
     reviewText=models.TextField() #리뷰 댓글
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # User와의 Foreign Key
