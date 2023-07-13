3a337c9554f35860f3eb65e502d71d9f5ae9dae8
whitecastle20 <whitecastle20@github.com>
2023-07-14 00:07:33 +0900

    user login service

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import login_view, signup_view, logout_view, like_list_view
+#from .views import login_view, signup_view, logout_view, like_list_view
+from .views import login_view, signup_view, logout_view, like_list_view, main2, consulting_view
 
 app_name = "user"
 
 urlpatterns = [
     path("login/", login_view, name='user-login'),
+    path("main2/", main2, name='user-main2'),
+    path("consulting/", consulting_view, name='user-consulting'),
     path("signup/", signup_view, name='user-signup'),
     path("logout/", logout_view, name='user-logout'),
     path('<int:id>/like/', like_list_view, name='user-like'),
--- user/views.py
+++ user/views.py
@@ -1,16 +1,30 @@
+
 from django.shortcuts import render, redirect
 
 from django.contrib import auth
 from .forms import UserForm
 from django.contrib.auth import authenticate, login
 from posts.models import Post
-def login_view(request):
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+def login_view(request): # 로그인
     if request.method == "GET":
         return render(request, 'user/login.html')
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            #return HttpResponseRedirect(reverse('main2')) # 성공
+            return HttpResponseRedirect(reverse('user:user-main2')) # 성공
+        else:
+            return render(request, 'user/login.html') #실패
 
 
-
-def signup_view(request):
+def signup_view(request): # 회원가입
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -19,7 +33,8 @@ def signup_view(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
             login(request, user)  # 로그인
-            return redirect('main2')
+            #return redirect('main2')
+            return redirect('user:user-main2')
     else:
         form = UserForm()
     return render(request, 'user/signup.html', {'form': form})
@@ -27,8 +42,13 @@ def signup_view(request):
 def main1(request): # 로그인 전 페이지
     return render(request, 'main1.html')
 
+def consulting_view(request): # 컨설팅 페이지
+    if request.method == 'POST':
+        return render(request, 'user/consulting.html')
+
+
 def main2(request): # 로그인 후  페이지
-    return render(request, 'main2.html')
+    return render(request, 'user/main2.html')
 
 
 def logout_view(request):
@@ -41,7 +61,7 @@ def logout_view(request):
 #             'likeList': likeList,
 #         }
 #         return render(request, 'user/saveList.html', context)
-def like_list_view(request, id):
+def like_list_view(request, id): # 마이페이지 찜 목록 보기
     likeList = Post.objects.filter(like=request.user.id)
     context = {
         'likeList': likeList,
