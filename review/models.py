64aab35449a00ea90ebe8ad1bf71ece723f9b02e
whitecastle20 <whitecastle20@github.com>
2023-07-14 09:07:31 +0900

    feat: review CRUD and design work

--- review/models.py
+++ review/models.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 class Review(models.Model):
     #post = models.ForeignKey('posts.Post', on_delete=models.CASCADE) # 글
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
     reviewTitle = models.TextField(max_length=50, default="제목 없음")  # 리뷰 제목
     reviewText = models.TextField() #리뷰 댓글
     created_at = models.DateTimeField(auto_now_add=True, null=True)
